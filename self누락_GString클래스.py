7a71d912794b320b86af99a053b2e0e859eb60ba
chaekhan <chaekhan@naver.com>
2024-08-27 13:44:25 +0900

    교육내용

--- self누락_GString클래스.py
+++ self누락_GString클래스.py
@@ -1,12 +1,14 @@
+# 전역변수 초기화
 strName = "Not Class Member"
 
 class DemoString:
     def __init__(self):
+        #인스턴스 맴버 변수
         self.strName = "" 
     def set(self, msg):
         self.strName = msg
     def print(self):
-        print(str)
+        print(self.strName)
 
 d = DemoString()
 d.set("First Message")
